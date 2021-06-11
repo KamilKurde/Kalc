2356494735ab653293c233a0c277128e72e58d6c
Kamil Bąk (Uczeń) <kbak281@technikumkreatywne.pl>
2021-06-11 12:58:58 +0200

    moved BigDecimal extension functions into one file

--- src/commonMain/kotlin/com/KamilKurde/Kalc/functions/bigDecimalExtensions.kt
+++ src/commonMain/kotlin/com/KamilKurde/Kalc/functions/bigDecimalExtensions.kt
@@ -2,4 +2,5 @@ package com.kamilKurde.kalc.functions
 
 import com.ionspin.kotlin.bignum.decimal.BigDecimal
 
+fun BigDecimal.Companion.parseNumber(number: Number) = BigDecimal.parseString(number.toString())
 fun BigDecimal.readable() = this.toStringExpanded()
